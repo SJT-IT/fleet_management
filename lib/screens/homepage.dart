bc2510ab26b518e629961cbc3fb5c45473191e8c
SJT-IT <it@sjtev.com>
2026-03-24 10:26:01 +0530

    Added role based routing

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,11 +1,71 @@
+import 'package:fleet_management/screens/roles/admin.dart';
+import 'package:fleet_management/screens/roles/dealer.dart';
+import 'package:fleet_management/screens/roles/driver.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   void _logout() async {
     await FirebaseAuth.instance.signOut();
+  }
+
+  Future<String?> getUserRole() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) return null;
+
+    final doc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get();
+
+    if (doc.exists) {
+      return doc['role']; // e.g. "Admin", "Dealer", "Driver"
+    }
+
+    return null;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _checkUserRole();
+  }
+
+  Future<void> _checkUserRole() async {
+    final role = await getUserRole();
+
+    if (!mounted) return;
+
+    if (role == "Admin") {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const AdminScreen()),
+      );
+    } else if (role == "Dealer") {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const DealerScreen()),
+      );
+    } else if (role == "Driver") {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const DriverScreen()),
+      );
+    } else {
+      // fallback if role missing
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Role not assigned")));
+    }
   }
 
   @override
